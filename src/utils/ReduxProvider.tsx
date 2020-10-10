a66d8a0a59e2fde657ad858bfeb4cc412a2faa61
Tinsson <695724046@qq.com>
2020-10-10 08:55:39 +0800

    fix ts

---
+++ src/utils/ReduxProvider.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+interface User {
+  name: string,
+  phone: string,
+}
+
+interface StateType {
+  searchWords: string,
+  userList: User[]
+}
+
+type ActionType = {
+  type: 'UPDATE_SEARCH',
+  payload: string
+} | {
+  type: 'UPDATE_LIST',
+  payload: User[]
+}
+
+// 创建初始化state，dispatch
+const initState: StateType = {
+  searchWords: '',
+  userList: []
+}
+
+// 定义reducer分发规则
+const reducer = (state: StateType = initState, action: ActionType): StateType => {
+  switch (action.type) {
+    case 'UPDATE_SEARCH':
+      return {
+        ...state,
+        searchWords: action.payload
+      };
+    case 'UPDATE_LIST':
+      return {
+        ...state,
+        userList: action.payload
+      };
+    default:
+      break;
+  }
+  return state
+}
+
+const store = createStore(reducer, initState)
+
+const ReduxProvider: React.FC<{ children: JSX.Element }> = ({
+  children
+}) => {
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  )
+}
+
+export default ReduxProvider
